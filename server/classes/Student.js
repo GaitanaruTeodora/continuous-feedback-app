c4a47b0a681922a0a96603eff3d2af04aad6a0a6
unknown <teo.gaitanaru@gmail.com>
2021-12-02 23:19:34 +0200

    delete classes

--- server/classes/Student.js
+++
@@ -1,9 +0,0 @@
-class Student{
-    constructor(id,nume,prenume,grupa){
-        this.id = id;
-        this.nume = nume;
-        this.prenume = prenume;
-        this.grupa = grupa;
-    }
-}
-module.exports = Student
